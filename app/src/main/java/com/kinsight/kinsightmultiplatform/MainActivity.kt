bdef1658bcd11b2fb64ac1895069b8a99a68ee15
Dmitri <dmitrinyc@yahoo.com>
2019-10-16 17:20:06 -0400

    basic read from android, installed ktx lifecycle deps

--- app/src/main/java/com/kinsight/kinsightmultiplatform/MainActivity.kt
+++ app/src/main/java/com/kinsight/kinsightmultiplatform/MainActivity.kt
@@ -3,6 +3,10 @@ package com.kinsight.kinsightmultiplatform
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.kinsight.kinsightmultiplatform.api.IdeaApi
+import com.kinsight.kinsightmultiplatform.repository.IdeaRepository
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
+
+        var ideaRep = IdeaRepository()
+
+        lifecycleScope.launch {
+            val result = ideaRep.fetchIdeas();
+            print(result)
+        }
+
+        //var data = ideaRep.fetchIdeas()
     }
 }
