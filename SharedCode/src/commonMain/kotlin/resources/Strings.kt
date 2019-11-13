06f8f038b77e66c11a694c26fb5711b9ef05ff0c
dmitrish <dmitrinyc@yahoo.com>
2019-11-12 23:02:35 -0500

    allowClearTextTraffic (for local server communication)

--- SharedCode/src/commonMain/kotlin/resources/Strings.kt
+++ SharedCode/src/commonMain/kotlin/resources/Strings.kt
@@ -10,5 +10,18 @@ object Strings {
  val SERVER_URL_LOCAL_BASE_FOR_EMULATOR = "10.0.0.2"
  val SERVER_URL_REMOTE_BASE = "https://alphacapture.appspot.com"
  val PORT = "8081"
+ val title_activity_create_new_idea = "CreateNewIdeaActivity"
+ val navigation_drawer_open = "Open navigation drawer"
+ val navigation_drawer_close = "Close navigation drawer"
+ val nav_header_title = "Android Studio"
+ val nav_header_subtitle = "android.studio@android.com"
+ val nav_header_desc = "Navigation header"
+ val action_settings = "Settings"
+ val menu_home = "Home"
+ val menu_gallery = "Gallery"
+ val menu_slideshow = "Slideshow"
+ val menu_tools = "Tools"
+ val menu_share = "Share"
+ val menu_send = "Send"
 }
             
