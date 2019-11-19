4f5ca30e1afc6d534701d2832537a72444cadb17
Piyush Chhabra <piyushchhabra007@gmail.com>
2019-11-19 10:11:46 -0500

    handling emplty string scenario

--- native/KotlinIOS/KotlinIOS/ViewModels/TickerSearchViewModel.swift
+++ native/KotlinIOS/KotlinIOS/ViewModels/TickerSearchViewModel.swift
@@ -21,7 +21,7 @@ public class TickerSearchViewModel : ObservableObject {
     
     @Published var tickers = [TickerModel]()
     @Published var dataRequestInProgress = ProgressModel()
-    @Published var searchText = "aapl"
+    @Published var searchText = ""
     
     private var searchSubscriber: AnyCancellable!
       private var disposables = Set<AnyCancellable>()
@@ -31,13 +31,18 @@ public class TickerSearchViewModel : ObservableObject {
     init(repository: IdeaRepository) {
         self.repository = repository
         searchSubscriber = $searchText.removeDuplicates()
-            .debounce(for: 0.8, scheduler: DispatchQueue.main)
+            .debounce(for: 0.5, scheduler: DispatchQueue.main)
             .sink(receiveValue: fetchTickers(for:))
     }
     
     
     func fetchTickers(for tickerString: String){
         dataRequestInProgress.inProgress = true
+        if tickerString.isEmpty {
+            self.tickers = []
+        self.dataRequestInProgress.inProgress = false
+            return
+        }
         
         self.repository?.fetchTickers(tickerFilter: tickerString, success: { (tickerModels) in
             
