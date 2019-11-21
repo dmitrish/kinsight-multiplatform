fa4905682b159979a59880a5ed3d4afdd038ea8a
dmitrish <dmitrinyc@yahoo.com>
2019-11-20 19:03:59 -0500

    work in activity create in progress

--- app/src/main/java/com/kinsight/kinsightmultiplatform/views/IdeaActivity.kt
+++ app/src/main/java/com/kinsight/kinsightmultiplatform/views/IdeaActivity.kt
@@ -1,28 +1,10 @@
 package com.kinsight.kinsightmultiplatform.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kinsight.kinsightmultiplatform.R
 import kotlinx.android.synthetic.main.activity_idea.*
-import kotlinx.android.synthetic.main.idea_item.*
-import kotlinx.android.synthetic.main.idea_item.view.*
 import java.math.RoundingMode
 import java.text.DecimalFormat
-import android.view.View.X
-import android.widget.LinearLayout
-
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.view.animation.LinearInterpolator
-import android.view.animation.Animation
-import android.view.animation.RotateAnimation
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
-
-
-
 
 
 
@@ -37,7 +19,7 @@ class IdeaActivity : FullScreenActivity() {
         val ticker = startIntent.getStringExtra("ideaTicker")
         val alpha = startIntent.getDoubleExtra("ideaAlpha", 0.0)
         val createdBy = startIntent.getStringExtra("ideaCreatedBy")
-        val targetPrice = startIntent.getStringExtra("ideaTargetPrice")
+        val targetPrice = startIntent.getDoubleExtra("ideaTargetPrice", 0.0)
 
         ideaCompany.text = companyName
         ideaTicker.text = ticker
@@ -46,25 +28,46 @@ class IdeaActivity : FullScreenActivity() {
         df.roundingMode = RoundingMode.CEILING
         val alphaFormatted = df.format(alpha)
         alphaValue.text = alphaFormatted
+        val targetFormatted = df.format(targetPrice)
+        ideaDetailCurrentPrice.text ="$${targetFormatted}"
+        ideaDetailTargetPrice.text ="$${targetFormatted}"
 
         if (alpha >= 4){
             alphaLabl.setImageResource(R.drawable.ic_fish_superhot)
        }
-        else if (alpha.toDouble() >= 3 && alpha.toDouble() < 4){
+        else if (alpha >= 3 && alpha < 4){
             alphaLabl.setImageResource(R.drawable.ic_fish_blue)
         }
-        else if (alpha.toDouble() >= 1 && alpha.toDouble() < 3){
+        else if (alpha >= 1 && alpha < 3){
             alphaLabl.setImageResource(R.drawable.ic_fish_red)
         }
-        if (alpha.toDouble() < 1){
+        if (alpha < 1){
             alphaLabl.setImageResource(R.drawable.ic_fish_onfire)
         }
 
+
+
+       /* val path = Path().apply {
+            arcTo(0f, 0f, 1000f, 1000f, 270f, -180f, true)
+        }
+        val pathInterpolator = PathInterpolator(path)
+
+
+        */
         alphaLabl.alpha = 0F
         alphaLabl.animate().apply {
+            duration = 3000
             alpha(1f)
             start()
         }
+
+       /* val animation = ObjectAnimator.ofFloat(alphaLabl, "translationX", 100f).apply {
+            interpolator = pathInterpolator
+            start()
+        }
+
+        */
+
 
     }
 }
